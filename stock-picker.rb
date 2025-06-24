7f711e3afb4a680b2c6c584f5553549691c6e5cb
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2025-06-24 16:12:14 -0400

    Implement stock_picker method

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,4 +1,11 @@
 def stock_picker(stock_prices)
   # Take in an array of stock prices
   # Return a pair of days representing the best day to buy and the best day to sell
+  day_indexes = (0...stock_prices.length).to_a
+  stock_returns = day_indexes.combination(2).to_h do |day_pair| 
+    stock_return = stock_prices[day_pair.last] - stock_prices[day_pair.first]
+    [day_pair, stock_return]
+  end
+
+  stock_returns.max_by { |key, value| value }.first
 end
